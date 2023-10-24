bb8f2c1fe08cf855412f63953613a87938760866
nudieisgood <nudieisgoodmii@gmail.com>
2023-10-24 14:12:49 +0800

    test deploy

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import placesRoute from "./routes/placesRoute.js";
 import bookingRoute from "./routes/bookingRoute.js";
+const abc = "abc";
 
 //middleware
 import { authenticateUser } from "./middlewares/authMiddleware.js";
